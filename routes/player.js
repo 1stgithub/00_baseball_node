83300359b7c7d2ff94de624b3889ae13b799f3ce
1stgithub <53161387+1stgithub@users.noreply.github.com>
2019-08-26 20:07:49 -0700

    baseball app with no pictures

--- routes/player.js
+++ routes/player.js
@@ -8,9 +8,11 @@ module.exports = {
         });
     },
     addPlayer: (req, res) => {
+        /*
         if (!req.files) {
             return res.status(400).send("No files were uploaded.");
         }
+        */
 
         let message = '';
         let first_name = req.body.first_name;
@@ -18,10 +20,10 @@ module.exports = {
         let position = req.body.position;
         let number = req.body.number;
         let username = req.body.username;
-        let uploadedFile = req.files.image;
-        let image_name = uploadedFile.name;
-        let fileExtension = uploadedFile.mimetype.split('/')[1];
-        image_name = username + '.' + fileExtension;
+        //let uploadedFile = req.files.image;
+        //let image_name = uploadedFile.name;
+        //let fileExtension = uploadedFile.mimetype.split('/')[1];
+        //image_name = username + '.' + fileExtension;
 
         let usernameQuery = "SELECT * FROM `players` WHERE user_name = '" + username + "'";
 
@@ -33,36 +35,39 @@ module.exports = {
                 message = 'Username already exists';
                 res.render('add-player.ejs', {
                     message,
-                    title: "Welcome to the Baseball Database | Add a new player"
+                    title: "Welcome to Socka | Add a new player"
                 });
             } else {
                 // check the filetype before uploading it
-                if (uploadedFile.mimetype === 'image/png' || uploadedFile.mimetype === 'image/jpeg' || uploadedFile.mimetype === 'image/gif') {
+                /*if (uploadedFile.mimetype === 'image/png' || uploadedFile.mimetype === 'image/jpeg' || uploadedFile.mimetype === 'image/gif')*/ {
                     // upload the file to the /public/assets/img directory
-                    uploadedFile.mv(`public/assets/img/${image_name}`, (err ) => {
-                        if (err) {
+                    /*uploadedFile.mv(`public/assets/img/${image_name}`, (err ) => */{
+                        /* if (err) {
                             return res.status(500).send(err);
-                        }
+                        }*/
                         // send the player's details to the database
-                        let query = "INSERT INTO `players` (first_name, last_name, position, number, image, user_name) VALUES ('" +
-                            first_name + "', '" + last_name + "', '" + position + "', '" + number + "', '" + image_name + "', '" + username + "')";
+                        //let query = "INSERT INTO `players` (first_name, last_name, position, number, image, user_name) VALUES ('" +
+                            //first_name + "', '" + last_name + "', '" + position + "', '" + number + "', " + image_name + "', '" + username + "')";
+                        let query = "INSERT INTO `players` (first_name, last_name, position, number, user_name) VALUES ('" +
+                            first_name + "', '" + last_name + "', '" + position + "', '" + number + "', '" + username + "')";
                         db.query(query, (err, result) => {
                             if (err) {
                                 return res.status(500).send(err);
                             }
                             res.redirect('/');
                         });
-                    });
-                } else {
+                    }//);
+                } /*else {
                     message = "Invalid File format. Only 'gif', 'jpeg' and 'png' images are allowed.";
                     res.render('add-player.ejs', {
                         message,
                         title: "Welcome to the Baseball Database | Add a new player"
                     });
-                }
+                }*/
             }
         });
     },
+
     editPlayerPage: (req, res) => {
         let playerId = req.params.id;
         let query = "SELECT * FROM `players` WHERE id = '" + playerId + "' ";
@@ -94,28 +99,31 @@ module.exports = {
     },
     deletePlayer: (req, res) => {
         let playerId = req.params.id;
-        let getImageQuery = 'SELECT image from `players` WHERE id = "' + playerId + '"';
+        //let getImageQuery = 'SELECT image from `players` WHERE id = "' + playerId + '"';
         let deleteUserQuery = 'DELETE FROM players WHERE id = "' + playerId + '"';
-
-        db.query(getImageQuery, (err, result) => {
+        
+        
+        /*db.query(getImageQuery, (err, result) => */{
+            /*
             if (err) {
                 return res.status(500).send(err);
             }
+            */
 
-            let image = result[0].image;
+            //let image = result[0].image;
 
-            fs.unlink(`public/assets/img/${image}`, (err) => {
-                if (err) {
-                    return res.status(500).send(err);
-                }
+            /*fs.unlink(`public/assets/img/${image}`, (err) => */{
+                //if (err) {
+                //    return res.status(500).send(err);
+                //}
                 db.query(deleteUserQuery, (err, result) => {
                     if (err) {
                         return res.status(500).send(err);
                     }
                     res.redirect('/');
                 });
-            });
-        });
+            }//);
+        }//);
     }
 };
 
